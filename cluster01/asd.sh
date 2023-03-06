b3b29952a5fa66b03b02fd084eeff28a74ff49eb
Dodex <dodexpwnz@gmail.com>
2023-03-06 21:15:33 +0300

    install gitlab

--- cluster01/asd.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo iptables -t nat -A POSTROUTING -t nat -p tcp -m tcp -s 172.18.0.101 --sport 80 -j SNAT --to-source 0.0.0.0
-sudo iptables -A FORWARD -m state -p tcp -d 172.18.0.101 --dport 80 --state NEW,ESTABLISHED,RELATED -j ACCEPT
-sudo iptables -t nat -A PREROUTING -p tcp --dport 1080 -j DNAT --to-destination 172.18.0.101:80
-
-sudo iptables -t nat -A POSTROUTING -t nat -p tcp -m tcp -s 172.18.0.101 --sport 443 -j SNAT --to-source 0.0.0.0
-sudo iptables -A FORWARD -m state -p tcp -d 172.18.0.101 --dport 443 --state NEW,ESTABLISHED,RELATED -j ACCEPT
-sudo iptables -t nat -A PREROUTING -p tcp --dport 10443 -j DNAT --to-destination 172.18.0.101:443
